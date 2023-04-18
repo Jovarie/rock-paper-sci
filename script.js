17b6eb6bd5621d4c3a257f09fa493cf9c698a992
Jovarie <jojofyffe1@gmail.com>
2023-04-18 20:14:24 +0100

    add in more comments explaining the code and ai check certain lines as the x beats x display message dosent work properly also asl ai to explin anything back if it dosent make sense, if all fails start it all again

--- script.js
+++ script.js
@@ -1,96 +1,57 @@
-//begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-    const options = ["rock", "paper", "scissors"]
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('button')
 
-    //our game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-    function getComputerChoice(){
-        const choice = options[Math.floor(Math.random() * options.length)];
-        return choice;
-    }
+//computer choices
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+//this function disables the buttons and will run after a winner if declared i.e first to 5.
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
 
-    //checks who wins each round
-    function checkWinner(playerSelection, computerSelection) {
-        if (playerSelection == computerSelection) {
-            return "Tie";
-        }
-        else if (
-            (playerSelection == "rock" && computerSelection == "scissors") ||
-            (playerSelection == "scissors" && computerSelection == "paper") ||
-            (playerSelection == "paper" && computerSelection == "rock")
-        ){
-            return "Player";
-        }
-        else {
-            return "Computer";
-        }
-    }
-    
-
-    //Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection
-    function playRound(playerSelection, computerSelection) {
-        const result = checkWinner(playerSelection, computerSelection);
-        if (result == "Tie"){
-            return "It's a Tie!";
-        }
-        else if (result == "Player"){
-            return `You Win! ${playerSelection} beats ${computerSelection}`
-        }
-        else {
-            return `You Lost! ${computerSelection} beats ${playerSelection}`
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
+    //game logic
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        //if player wins add 1 to their score and display a message
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+        //if player gets to 5 display this message
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
         }
     }
-
-    //function to get Users input.
-    function getPlayerChoice(){
-        //while this input is false keep looping.
-        let validatedInput = false;
-        while(validatedInput == false) {
-            const choice = prompt("Rock, Paper or Scissors?");
-            if (choice == null) {
-                continue;
-            }
-            //converts input to lowercase.
-            const choiceInLower = choice.toLowerCase();
-            //returns true if an array contains a specified element.
-            if (options.includes(choiceInLower)) {
-                validatedInput = true;
-                return choiceInLower;
-                //this will stop the loop.
-            }
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    }//if computer wins display this message
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+        //if computer wins the game display this message
+        if (computerScore == 5) {
+            result += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
         }
     }
-
-
-    //Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-    function game() {
-        //keeps score & declares a winner at game completion.
-        let scorePlayer = 0;
-        let scoreComputer = 0;
-
-
-        for (let i = 0; i < 5; i++) {
-            const playerSelection = getPlayerChoice();
-            const computerSelection = getComputerChoice();
-            console.log(playRound(playerSelection, computerSelection));
-            console.log("----------")//creates spaces so we know were in a different round.
-            //checks for a winner and updates their score
-            if (checkWinner(playerSelection, computerSelection) == "Player") {
-                scorePlayer++;
-            }
-            else if (checkWinner(playerSelection, computerSelection == "Computer")) {
-                scoreComputer++;
-            }
-         }
-         console.log("Game Over");
-         //checks for and logs the winner
-         if (scorePlayer > scoreComputer) {
-            console.log("Player was the winner!");
-         }
-         else if (scorePlayer < scoreComputer) {
-            console.log("Computer was the winner!");
-         }
-         else if (scorePlayer == scoreComputer) {
-            console.log("We have a tie!");
-         }
-    }
-
-    game(); //calls 5 round game.
+//displays the game result in the result div and returns it
+    document.getElementById('result').innerHTML = result
+    return
+}
+// added a click event listener to the button...(finish this comment and rest of comments to retry again)
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
