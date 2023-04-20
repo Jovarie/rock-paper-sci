b4870b31edd6ad0f157a8b57c17f4437eb376720
Jovarie <jojofyffe1@gmail.com>
2023-04-20 10:10:43 +0100

    added comments explaining code for later evaluation, completed simple rock-paper-scissors game with minor DOM manipulation, used vanilla JS as well as online tools like ai stack overflow and youtube to solve small bugs and errors, next ill attempt to make a complete UI interface with moving objects which provide animation

--- script.js
+++ script.js
@@ -14,18 +14,22 @@ function disableButtons() {
     })
 }
 
+//his function is called every time a button is clicked. It takes in the playerSelection as an argument 
 function playRound(playerSelection) {
+//Generates a random computerSelection using the computerPlay() function.
     let computerSelection = computerPlay()
     let result = ""
     //game logic
+    //compares the player selection and computer selection and compares each outcome to determine a winner
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection == 'rock')) {
         //if player wins add 1 to their score and display a message
+        //updates the score of each computer and player
         playerScore += 1
         result = ('You win! ' + playerSelection + ' beats ' + computerSelection
             + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
-        //if player gets to 5 display this message
+        //if player gets to 5 display this message if either player reaches a score of 5, displays a message declaring the winner and disables all buttons on the page using the disableButtons() function.
         if (playerScore == 5) {
             result += '<br><br>You won the game! Reload the page to play again'
             disableButtons()
@@ -45,11 +49,11 @@ function playRound(playerSelection) {
             disableButtons()
         }
     }
-//displays the game result in the result div and returns it
+//Updates the HTML content of the result element on the page with the message generated and returns the result.
     document.getElementById('result').innerHTML = result
     return
 }
-// added a click event listener to the button...(finish this comment and rest of comments to retry again)
+//Event listener: The forEach method is used to add a click event listener to each button on the page. When a button is clicked, the playRound(playerSelection) function is called with the value attribute of the button as its argument.
 buttons.forEach(button =>{
     button.addEventListener('click', function(){
         playRound(button.value)
